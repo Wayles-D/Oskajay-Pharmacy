6345f947e2984edac67d220f0dcf3e0e95858dd0
Danyel222 <waylesstores@gmail.com>
2025-08-29 03:27:13 +0100

    Refactor main.jsx to remove commented-out code and maintain clean structure

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
-
-// import React from "react";
-// import ReactDOM from "react-dom/client"
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//   <App />
-//   </BrowserRouter>
-// )
